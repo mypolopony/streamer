614a23424545828036aaf8b76302be751fc191be
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2019-02-15 12:38:08 -0800

    Delete credentials.py

--- credentials.py
+++
@@ -1,5 +0,0 @@
-key = 'cok4sOTC7tX3J5ughI3rFpbSx'
-secret = 'QSia5RKFdloXDi6DUq2MwnpGP49eSTCtySvljIJqYh4fLS0vmj'
-
-authkey = '1217553746-TuFJgNC79p2uoAPlstnqkM9HoTOb7NsEkVmIGYx'
-authsecret = 'X7jB10iBsgQrnTN9O1T8k7R7Ii8ZdNGAnMRAil3XU8CUS'
