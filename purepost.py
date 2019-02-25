5a0db8731f0fea3ff55c8020515440e8c70afc29
Selwyn-Lloyd <selwyn@agridata.ai>
2019-02-24 17:45:53 -0800

    complete posting queue

--- purepost.py
+++ purepost.py
@@ -4,11 +4,12 @@
 # @Author: Selwyn-Lloyd
 # @Date:   2019-02-15 13:11:16
 # @Last Modified by:   Selwyn-Lloyd
-# @Last Modified time: 2019-02-24 16:41:53
+# @Last Modified time: 2019-02-24 17:43:47
 
 import os
 import sys
 import time
+import random
 import datetime
 import credentials          # Non-gitted credentials
 from pymongo import MongoClient
@@ -42,9 +43,9 @@ if __name__ in ('__console__', '__main__'):
 
     # Add
     if mode == 'add':
-        post = ' '.join(sys.argv[2:])
+        post = '"' + ' '.join(sys.argv[2:]) + '"'
 
-        db.queue.insert({'text': post})
+        db.queue.insert_one({'text': post})
         pprint('Added to Queue: {}'.format(post))
 
         sys.exit(0)
@@ -54,13 +55,14 @@ if __name__ in ('__console__', '__main__'):
         pprint('Running. . .')
 
         while True:
-            post = db.queue.get_one()
+            post = db.queue.find_one()
             if post:
                 api.update_status(post['text'])
-                pprint('Inserted: {}'.format(post['text']))
+                pprint('Posted: {}'.format(post['text']))
+                db.queue.delete_one({'_id': post['_id']})
 
             # Two hours +/- 60 minutes
             seconds = 2 * 60 * 60 + 60 * random.randint(0, 60)
-            pprint('Next update in {} minutes': seconds / 60)
+            pprint('Next update in {} minutes'.format(int(seconds / 60)))
                     
             time.sleep(seconds)
