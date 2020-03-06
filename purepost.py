6277666c3164964a3ab9fd64777f3fc1f3d12fd7
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2020-03-06 12:56:30 -0800

    more purepost

--- purepost.py
+++ purepost.py
@@ -1,10 +1,18 @@
-#!/usr/local/opt/python/bin/python3.7
-
 # -*- coding: utf-8 -*-
 # @Author: Selwyn-Lloyd
 # @Date:   2019-02-15 13:11:16
-# @Last Modified by:   Selwyn-Lloyd
-# @Last Modified time: 2019-02-24 17:43:47
+# @Last Modified by:   mypolopony
+# @Last Modified time: 2020-02-05 20:18:41
+
+'''
+I often find turns of phrase that I think are pithy enough to etch 
+them into the twitter machine. While consuming media, it is easy
+to record these / copy and paste in a flat file, which I generally
+always have open.
+
+Wanting to post these brilliant finds, I use the Tweepy API to 
+post them automatically at a reasonable pace,   
+'''
 
 import os
 import sys
@@ -16,9 +24,31 @@ from pymongo import MongoClient
 from tweepy import OAuthHandler, API
 
 def pprint(msg):
+    '''
+    Our own special pretty-print
+    '''
     print('{}: {}'.format(datetime.datetime.now().strftime('%c'), msg))
 
-if __name__ in ('__console__', '__main__'):
+# Database connection
+c = MongoClient()
+db = c['streamer']
+pprint('Database connection successful')
+
+# Twitter authentication
+auth = OAuthHandler(credentials.key, credentials.secret)
+auth.set_access_token(credentials.authkey, credentials.authsecret)
+pprint('Authenticated')
+
+# Twitter / Tweepy API
+api = API(auth)
+pprint('API connected')
+
+def console_input():
+    '''
+    Add via command line (possible direct injection, but 
+    not necessarily recommended)
+    '''
+
     # Check arguments
     if len(sys.argv) == 1 or sys.argv[1] not in ('add', 'run'):
         pprint('Use an argument: add or run')
@@ -27,21 +57,7 @@ if __name__ in ('__console__', '__main__'):
         # Mode
         mode = sys.argv[1]
 
-    # Database connection
-    c = MongoClient()
-    db = c['streamer']
-    pprint('Database connection successful')
-
-    # Twitter authentication
-    auth = OAuthHandler(credentials.key, credentials.secret)
-    auth.set_access_token(credentials.authkey, credentials.authsecret)
-    pprint('Authenticated')
-
-    # Twitter / Tweepy API
-    api = API(auth)
-    pprint('API connected')
-
-    # Add
+    # Add via command line 
     if mode == 'add':
         post = '"' + ' '.join(sys.argv[2:]) + '"'
 
@@ -50,19 +66,34 @@ if __name__ in ('__console__', '__main__'):
 
         sys.exit(0)
 
-    # Monitor
-    elif mode == 'run':
-        pprint('Running. . .')
 
-        while True:
-            post = db.queue.find_one()
-            if post:
-                api.update_status(post['text'])
-                pprint('Posted: {}'.format(post['text']))
-                db.queue.delete_one({'_id': post['_id']})
+# Main things
+if __name__ in ('__console__', '__main__'):
+    pprint('Tervetuloa! Welcome!')
 
-            # Two hours +/- 60 minutes
-            seconds = 2 * 60 * 60 + 60 * random.randint(0, 60)
-            pprint('Next update in {} minutes'.format(int(seconds / 60)))
-                    
-            time.sleep(seconds)
+    # Populate queue using file (insensitive to redundancy)
+    pprint('Checking TWEETME file for new entries')
+    try:
+        with open('TWEETME', 'r') as filequeue:
+            for line in filequeue:
+                if not db.queue.find_one({'text': line}):
+                    db.queue.insert_one({'text': line, 'posted': False})
+    except:
+        # I think there may be a situation in which there is no file
+        pass
+
+
+    # Post loop
+    while True:
+        post = db.queue.find_one({'posted': False})
+        if post:
+            pprint('There\'s a new')
+            api.update_status(post['text'])
+            pprint('Posted: {}'.format(post['text']))
+            db.queue.update_one({'_id': post['_id']}, {'$set': {'posted': True}})
+
+        # Wait for two hours +/- 60 minutes, as if it weren't a bot
+        seconds = 2 * 60 * 60 + 60 * random.randint(0, 60)
+        pprint('Next update in {} minutes'.format(int(seconds / 60)))
+                
+        time.sleep(seconds)
