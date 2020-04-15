51a0bfcc8aa552a949df10ef9cbafb5d4376cd78
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2020-04-14 19:31:53 -0700

    Better Roadmap, a la Frost

--- purepost.py
+++ purepost.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Selwyn-Lloyd
 # @Date:   2019-02-15 13:11:16
-# @Last Modified by:   mypolopony
-# @Last Modified time: 2020-03-21 11:30:31
+# @Last Modified by:   Selwyn-Lloyd McPherson
+# @Last Modified time: 2020-04-14 19:31:16
 
 '''
 I often find turns of phrase that I think are pithy enough to etch 
@@ -19,13 +19,13 @@ import sys
 import time
 import random
 import datetime
-import credentials          # Non-gitted credentials
+import credentials                      # Non-gitted credentials
 from pymongo import MongoClient
 from tweepy import OAuthHandler, API
 
 def pprint(msg):
     '''
-    Our own special pretty-print
+    Our own special pretty-print, because why not?
     '''
     print('{}: {}'.format(datetime.datetime.now().strftime('%c'), msg))
 
@@ -43,10 +43,17 @@ pprint('Authenticated')
 api = API(auth)
 pprint('API connected')
 
+def announce();
+    '''
+    Just basic status check
+    '''
+    pass
+
+
+
 def console_input():
     '''
-    Add via command line (possible direct injection, but 
-    not necessarily recommended)
+    Add via command line (direct addition to the database)
     '''
 
     # Check arguments
@@ -67,47 +74,63 @@ def console_input():
         sys.exit(0)
 
 
+def update_archives():
+    '''
+    Fortunately, or unfortunately, there are a number of sources based 
+    on the constant evolution of this project. They are:
+    '''
+
+    '''
+    1. Most recent (i.e. RAM) text file of intended posts and old raw text files 
+        of intended posts
+    '''
+
+    txtsources = ['TWEETME', 'TWEETMEAGAIN']
+    for txtsource in txtsources:
+        with open(txtsource):
+            db.queue.insert_one({'_id': post['_id']}, {'$set': {'posted': True}})
+
+    '''
+    2. Partial restorations of thousands of copy-paste history, captured
+           by CopyClip, across two different versions, because I accidentally
+           overwrote the above RAM and previous-RAM
+            [these are exports from the raw CopyClip sqlite database; /db1 and /db2], 
+                and yield JSONs
+    ** This collection needs to be vetted, as not all copy-paste is a source
+
+    '''
+
+    # -- Input
+
+    # -- Curation
+
+    # -- Output / Registration
+
+    
+
+
 # Main things
 if __name__ in ('__console__', '__main__'):
     pprint('Tervetuloa! Welcome!')
 
-    '''
-    Unfortunately, or fortunately, there are a number of sources based 
-    on the constant evolution of this project. They are:
-
-        1. Most recent (i.e. RAM) text file of intended posts
-            [currently TWEETMEAGAIN]
-        2. Old raw text files of intended posts
-            [e.g. TWEETME]
-        3. Partial restorations of thousands of copy-paste history, captured
-           by CopyClip, across two different versions, because I accidentally
-           overwrote the above RAM and previous-RAM
-            [these are exports from the raw CopyClip sqlite database;
-                /db1 and /db2], yielding jsons according to 
-
-
-    # Populate queue using file (insensitive to redundancy)
-    pprint('Checking TWEETME file for new entries')
-    try:
-        with open('TWEETME', 'r') as filequeue:
-            for line in filequeue:
-                if not db.queue.find_one({'text': line}):
-                    db.queue.insert_one({'text': line, 'posted': False})
-    except:
-        # I think there may be a situation in which there is no file
-        pass
-
-
-    # Post loop
     while True:
+        # Check for anything new and resolve the database
+        update_archives()
+
         post = db.queue.find_one({'posted': False})
         if post:
-            pprint('There\'s a new')
-            api.update_status(post['text'])
-            pprint('Posted: {}'.format(post['text']))
-            db.queue.update_one({'_id': post['_id']}, {'$set': {'posted': True}})
+            # Announce
+            pprint('There\'s a new post!: {}'.format(post['text']))
 
-        # Wait for two hours +/- 60 minutes, as if it weren't a bot
+            # Post
+            api.update_status(post['text'])
+
+            # Mark as posted
+            db.queue.update_one({'_id': post['_id']}, {'$set': {'posted': True}})
+            pprint('Posted: {}'.format(post['text']))
+            
+
+        # Wait for two hours +/- 60 minutes-ish, as if it weren't a bot
         seconds = 2 * 60 * 60 + 60 * random.randint(0, 60)
         pprint('Next update in {} minutes'.format(int(seconds / 60)))
                 
