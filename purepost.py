34aa284160aaf6b09b4d5e50a8789dacd3f3426a
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2020-07-13 02:01:51 -0700

    Comment cleanup

--- purepost.py
+++ purepost.py
@@ -26,6 +26,19 @@ from pymongo import MongoClient
 from tinydb import TinyDB, Query
 from tweepy import OAuthHandler, API
 
+# Twitter authentication
+auth = OAuthHandler(credentials.key, credentials.secret)
+auth.set_access_token(credentials.authkey, credentials.authsecret)
+pprint('Authenticated')
+
+# Twitter / Tweepy API
+api = API(auth)
+pprint('API connected')
+
+# Location of local tweet database
+# Using tinydb here, as it is perfect for these small projects. It's NoSQL
+# because I really hate SQL. I think it jus uses JSON for storage. . .
+tweetdb = 'tweetdb.json'
 
 def convert(seconds):
     '''
@@ -38,19 +51,6 @@ def pprint(msg):
     Our own special pretty-print
     '''
     print('{}: {}'.format(datetime.datetime.now().strftime('%c'), msg))
-
-
-# Twitter authentication
-auth = OAuthHandler(credentials.key, credentials.secret)
-auth.set_access_token(credentials.authkey, credentials.authsecret)
-pprint('Authenticated')
-
-# Twitter / Tweepy API
-api = API(auth)
-pprint('API connected')
-
-# Location of local tweet database
-tweetdb = 'tweetdb.json'
 
 def open_db():
     # Local tweet database
@@ -104,7 +104,7 @@ def load_extant():
         extant = json.load(extant_in)
     extant = [e['tweet']['full_text'] for e in extant['data']]
 
-    # 3525 tweets wow that's a lot. Confirmed by visiting Twitter, yikes. [June 17 2020]
+    # 3525 tweets [as of June 17 2020] -- wow that's a lot. 
     pprint('Found {} Existing Posts'.format(len(extant)))
 
     pprint('Creating Database / Table')
@@ -118,30 +118,25 @@ def load_extant():
 
     return
 
-
 # Main things
 if __name__ in ('__console__', '__main__'):
 
     pprint('Tervetuloa! Welcome!')
 
-    # Using tinydb here, as it is perfect for these small projects. It's NoSQL
-    # because I really hate SQL. I think it jus uses JSON for storage. . .
-
     '''
-    # ONE TIMER-ISH
+    # ONE TIMER (hopefully)
     # Initiate (this only needs to be done once). This will also load the extant
     # tweets
     if not os.path.exists(tweetdb):
         load_extant()
+    '''
 
-    # DO THIS OCCASIONALLY, manually add to TWEETME and then delete
-    # Load those in the TWEETME queue
+    # 
     if os.path.exists('TWEETME'):
         tweets = open_db()
     with open('TWEETME', 'r') as newtweets:
         for idx, line in enumerate(newtweets):
             tweets.insert({'text': line.replace('\n',''), 'posted': False})
-    '''
 
     # Tweets database
     tweets = open_db() 
