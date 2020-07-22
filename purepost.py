577e1848b5b2dd982f073e2d73c2b45c42165a54
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2020-07-22 05:32:37 -0700

    time_analysis: technical comparisons between re, parse, datetime, dateutils, dateparser

--- purepost.py
+++ purepost.py
@@ -26,6 +26,13 @@ from pymongo import MongoClient
 from tinydb import TinyDB, Query
 from tweepy import OAuthHandler, API
 
+
+def pprint(msg):
+    '''
+    Our own special pretty-print
+    '''
+    print('{}: {}'.format(datetime.datetime.now().strftime('%c'), msg))
+
 # Twitter authentication
 auth = OAuthHandler(credentials.key, credentials.secret)
 auth.set_access_token(credentials.authkey, credentials.authsecret)
@@ -46,18 +53,89 @@ def convert(seconds):
     '''
     return time.strftime("%H:%M:%S", time.gmtime(n))
 
-def pprint(msg):
-    '''
-    Our own special pretty-print
-    '''
-    print('{}: {}'.format(datetime.datetime.now().strftime('%c'), msg))
-
 def open_db():
     # Local tweet database
     db = TinyDB(tweetdb)
     tweets = TinyDB.table(db, 'tweets')
 
     return tweets
+
+def time_analysis():
+    '''
+    This is one of the most frustrating Pythonic experiences of all time.
+    This method is not intended to be used functunally, but only for analysis and practice.
+
+    We receive, from the main program, this. . . Keep in mind I use double quotes in-code
+    and single quotes for comments. . . Just my style I guess. . .
+    '''
+
+    raw_data = """Sun Jul 19 07:03:55 2020: Next update in 15622.07397139018 seconds (4.3 hours)
+    Sun Jul 19 11:24:18 2020: Next update in 15583.76668589194 seconds (4.3 hours)
+    Mon Jul 20 00:43:29 2020: Next update in 13892.442895423877 seconds (3.9 hours)
+    Mon Jul 20 05:00:53 2020: Next update in 17538.876418275577 seconds (4.9 hours)
+    Mon Jul 20 13:16:15 2020: Next update in 12281.47739731487 seconds (3.4 hours)
+    Mon Jul 20 16:41:13 2020: Next update in 20983.12822644447 seconds (5.8 hours)
+    Mon Jul 20 22:30:57 2020: Next update in 14408.528000559105 seconds (4.0 hours)
+    Tue Jul 21 05:14:16 2020: Next update in 18421.565193946757 seconds (5.1 hours)
+    Tue Jul 21 13:21:41 2020: Next update in 19052.686349454507 seconds (5.3 hours)
+    Tue Jul 21 18:39:15 2020: Next update in 20470.149045667447 seconds (5.7 hours)
+    Wed Jul 22 00:20:26 2020: Next update in 17994.227905193908 seconds (5.0 hours)""".splitlines()
+
+    '''
+    The large discontinuities due to closing the app, restarting the computer, whatever, but
+    it's easily parsible.
+
+    At this point, we are archeologists, so let's not bother with actual coding practices
+    '''
+
+    # Sample line
+    test_text = raw_data[0]
+
+    # Sanity check on the text parsing
+    try:
+        # Re says
+        import re
+        pattern = re.compile('(?P<timestamp>.+): Next update in (?P<a>.+) seconds \((?P<hours>.+) hours\)') 
+        re_result = re.search(pattern, text).groupdict()
+
+        # parse says 
+        import parse
+        pattern = '{timestamp}: Next update in {seconds} seconds ({hours} hours)'
+        p = parse.compile(pattern)
+        parse_result = p.parse(text).named()
+
+        # Are they the same? Dict comparison!
+        assert(re_result == parse_result)
+
+    except:
+        print('Sorry, your parser is broken [this shouldn\'t happen]')
+
+
+    # Sanity check on the date parsing
+    try:
+        # Dateparser says
+        import dateparser
+        dt_dateparser = dateparser.parse(test_text)
+
+        # Dateutil says
+        from dateutil import parser
+        dt_dateutil = parser.parse(test_text)
+
+        # Datetime says #### UNFINISHED HERE
+        from datetime import datetime, time
+        dt_datetime = datetime.strptime('2015-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
+
+        # What's the differene? Clearly nothing
+        assert(dt_dateparser = dt_dateutil = dt_datetime)
+    
+    except:
+        print('Sorry, your date-time interpreter is broken [this shouldn\'t happen]')
+
+
+    '''
+    After all of that, we just have to pick one, I guess. Everything above was just flossing, the real
+    question is: why do the time differences seem. . . different to me. . .
+    '''
 
 def console_input():
     '''
@@ -136,9 +214,9 @@ if __name__ in ('__console__', '__main__'):
     # database queue, and a flat file one Command+Tab away is comforting to me. Nevertheless,
     # these (pre-)queues posts have to be scooped up at some point. Let's just have
     # the user decide whether she wants to do it now or defer.
-    prequeue = ' TWEETME'
+    prequeue = 'TWEETME'
     if os.path.exists(prequeue):
-        if input('TWEETME File Found. Send to DB? ('y' for yes): ') == 'y':
+        if input('TWEETME File Found. Send to DB? ("y" for yes): ') == 'y':
             tweets = open_db()
             with open(prequeue, 'r') as newtweets:
                 for idx, line in enumerate(newtweets):
@@ -161,7 +239,6 @@ if __name__ in ('__console__', '__main__'):
 
         if post:
             _ = api.update_status(post['text'])
-            pprint('Post Successful: {}'.format(post['text']))
             tweets.update({'posted':True}, doc_ids = [post.doc_id])
 
         # Wait 3 - 6 hours
